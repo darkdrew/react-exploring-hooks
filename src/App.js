dc50692e4a311cb77b2c93194d8d4e3bf14dca21
darkdrew <andrewleong14@gmail.com>
2019-07-20 22:42:39 +0800

    Add initial todo state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-import Button from './Button';
+
+const Todo = ({ todo, index }) => {
+  return <div className="todo">{todo.text}</div>
+}
 
 function App() {
+  const [todos, setTodo] = useState([
+    {
+      text: 'Learn about React',
+      isCompleted: false
+    },
+    {
+      text: 'Meet friend for lunch',
+      isCompleted: false
+    },
+    {
+      text: 'Build cool todo app',
+      isCompleted: false
+    }
+  ]);
   return (
     <div className="App">
-     <Button />
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo key={index} index={index} todo={todo} />
+        ))}
+      </div>
     </div>
   );
 }
